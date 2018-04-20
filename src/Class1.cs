0f0b151bcd406b0dd881138132881f14e6e031db
Louis Ukiri <louisukiri@gmail.com>
2018-04-20 02:33:22 -0500

    add tests

---
+++ src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OoxmlParser
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace OoxmlParser.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(true);
+        }
+    }
+}
