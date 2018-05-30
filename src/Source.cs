ed4328d172f8a833366e515226ddb10d628d78a3
Louis Ukiri <louisukiri@gmail.com>
2018-05-30 18:40:04 -0500

    add source class

---
+++ src/Source.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace OoxmlToHtml
+{
+    public class Source
+    {
+        public static char EOL = '\n';
+        public static char EOF = '\0';
+        private StringReader reader;
+        private string line;
+        private int lineNumber;
+        private int currentPos;
+
+        public Source(StringReader reader)
+        {
+            lineNumber = 0;
+            currentPos = -2;
+            this.reader = reader;
+        }
+
+        public char currentChar()
+        {
+            if (currentPos == -2)
+            {
+                readLine();
+                return nextChar();
+            }
+            else if (line == null)
+            {
+                return EOF;
+            }
+            else if (currentPos == -1 || currentPos == line.Length)
+            {
+                return EOL;
+            }
+
+            return line[currentPos];
+        }
+    }
+}
