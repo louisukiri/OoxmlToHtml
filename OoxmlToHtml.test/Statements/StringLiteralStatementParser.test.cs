f9c77327f8cfe247265b38c3675018599b51323f
Louis Ukiri <louisukiri@gmail.com>
2018-06-17 19:22:26 -0500

    add tests for RunStatement, StringLiteralParser and TextStatementParser

---
+++ OoxmlToHtml.test/Statements/StringLiteralStatementParser.test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OoxmlToHtml.test.Helpers;
+
+namespace OoxmlToHtml.test.Statements
+{
+    [TestFixture]
+    public class StringLiteralStatementParser
+    {
+        [Test]
+        public void ShouldExpandTokenToIncludeAdjacentStringLiterals()
+        {
+
+            var input = @"<w:body>
+                    <w:p>
+                        <w:pPr></w:pPr>
+                        <w:r>                            
+                            <w:t xml:space=""preserve"">Abc louis</w:t>
+                        </w:r>
+                    </w:p>
+                    <w:p>
+                        <w:pPr></w:pPr>
+                        <w:r>                            
+                            <w:t xml:space=""preserve"">def louis</w:t>
+                        </w:r>
+                    </w:p>
+                </w:body>";
+
+            var node = TestHelper.ParseString(input);
+
+            var stringLiteralNode = node.Children[0].Children[1].Children[0].Children[0];
+            var stringLiteralNode2 = node.Children[1].Children[1].Children[0].Children[0];
+
+            Assert.AreEqual("Abc louis", stringLiteralNode.GetAttribute("Text"));
+            Assert.AreEqual("def louis", stringLiteralNode2.GetAttribute("Text"));
+        }
+    }
+}
---
+++ OoxmlToHtml.test/Statements/TextStatementParser.test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OoxmlToHtml.test.Helpers;
+
+namespace OoxmlToHtml.test.Statements
+{
+    [TestFixture]
+    public class TextStatementParser
+    {
+        [Test]
+        public void ShouldNotNestTextStatements()
+        {
+
+            var input = @"<w:body>
+                    <w:p>
+                      <w:pPr></w:pPr>
+                        <w:r>                            
+                            <w:t xml:space=""preserve"">Abc louis</w:t>
+                        </w:r>
+                    </w:p>
+                    <w:p>
+                      <w:pPr></w:pPr>
+                        <w:r>
+                            <w:t xml:space=""preserve"">22 louis</w:t>
+                        </w:r>
+                    </w:p>
+                    <w:p>
+                      <w:pPr></w:pPr>
+                        <w:r>
+                            <w:t xml:space=""preserve"">3 louis</w:t>
+                        </w:r>
+                    </w:p>
+                </w:body>";
+
+            var node = TestHelper.ParseString(input);
+
+            Assert.AreEqual(3, node.Children.Count);
+        }
+    }
+}
