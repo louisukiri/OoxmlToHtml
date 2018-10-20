3db789da393e387fc3af205deff66c69f6b9d987
Louis Ukiri <louisukiri@gmail.com>
2018-10-19 22:42:46 -0500

    [Fix] Forget about doing the code tag here

---
+++ OoxmlToHtml.test/Analyzers/TextAnalyzerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using OoxmlToHtml.Abstracts;
+using OoxmlToHtml.Analyzers;
+using OoxmlToHtml.test.Helpers;
+
+namespace OoxmlToHtml.test.Analyzers
+{
+    [TestFixture]
+    public class TextAnalyzerTest
+    {
+        private INode result;
+        [SetUp]
+        public void Setup()
+        {
+            var input = @"
+                <w:body>
+                    <w:p>
+                        <w:r>                            
+                            <w:t> ok jim, ok</w:t>
+                        </w:r>
+                    </w:p>
+                </w:body>
+            ";
+            var textAnalyzer = new TextAnalyzer();
+
+            var node = TestHelper.ParseString(input, true);
+            result = textAnalyzer.Analyze(node);
+        }
+        [Test]
+        public void ShouldPropagateTextAttributeToRun()
+        {
+            var paragraphNode = result.child.child;
+            Assert.AreEqual("ok jim, ok", paragraphNode.GetAttribute("Text"));
+        }
+    }
+}
---
+++ src/Analyzers/TextAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using OoxmlToHtml.Abstracts;
+
+namespace OoxmlToHtml.Analyzers
+{
+    public class TextAnalyzer : Analyzer
+    {
+        protected override INode Act(INode node)
+        {
+            return node;
+        }
+    }
+}
